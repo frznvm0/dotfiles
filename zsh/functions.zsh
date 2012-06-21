64abbdb01f67620943b2ce4150122474e25b834c
frznvm0 <frznvm0@gmail.com>
2012-06-21 07:43:50 +0200

    Fixed git branch name color

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,7 +4,7 @@ function mcd() {
 
 function git_prompt() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "on %{$fg_bold[yellow]%}${ref#refs/heads/}%{$reset_color%}$(git_dirty)"
+  echo "on %{$fg[blue]%}${ref#refs/heads/}%{$reset_color%}$(git_dirty)"
 }
 
 function git_dirty() {
