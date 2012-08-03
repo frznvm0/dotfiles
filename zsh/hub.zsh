0619ecdf87ef1bbdb14e70b1e400bfd092a67c26
frznvm0 <frznvm0@gmail.com>
2012-08-03 11:06:15 +0200

    Removed hub

--- zsh/hub.zsh
+++
@@ -1,5 +0,0 @@
-# Setup hub function for git, if it is available; http://github.com/defunkt/hub
-if [ "$commands[(I)hub]" ] && [ "$commands[(I)ruby]" ]; then
-  function git() { hub "$@" } # eval `hub alias -s zsh`
-  compdef hub=git
-fi
