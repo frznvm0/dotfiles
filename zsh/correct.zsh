be5eed503201bcadaf78690059bcd2cfccd4493d
frznvm0 <frznvm0@gmail.com>
2012-08-17 18:02:56 +0200

    Aliases, correction fixes for zsh

--- zsh/correct.zsh
+++ zsh/correct.zsh
@@ -1,15 +1,9 @@
 setopt correctall
 
 alias man='nocorrect man'
-alias mv='nocorrect mv'
-alias mkdir='nocorrect mkdir'
 alias gist='nocorrect gist'
 alias heroku='nocorrect heroku'
-alias pacman='nocorrect pacman'
-alias grep='nocorrect grep'
 alias pgrep='nocorrect pgrep'
 alias pkill='nocorrect pkill'
-alias killall='nocorrect killall'
 alias torify='nocorrect torify'
-alias type='nocorrect type'
 
