41cdd901d9bfbdce3f282a03d504ec683fd152f2
frznvm0 <frznvm0@gmail.com>
2013-06-15 14:17:56 +0200

    Added swap() to functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,6 +2,13 @@ function mcd() {
   mkdir -p "$1" && cd "$1"
 }
 
+function swap() {
+  holding=$(mktemp)
+  mv "$1" $holding
+  mv "$2" "$1"
+  mv $holding "$2"
+}
+
 function git_prompt() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "on %{$fg[blue]%}${ref#refs/heads/}%{$reset_color%}$(git_dirty)"
