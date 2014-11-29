d3455c966bc5d0b38072aa66dc00205660a73018
frznvm0 <frznvm0@gmail.com>
2014-11-29 20:35:26 +0100

    Reorganize history.zsh and correct.zsh

--- zsh/correct.zsh
+++ zsh/correct.zsh
@@ -1,9 +1,3 @@
-setopt correctall
-
-alias man='nocorrect man'
-alias gist='nocorrect gist'
-alias heroku='nocorrect heroku'
-alias pgrep='nocorrect pgrep'
-alias pkill='nocorrect pkill'
-alias torify='nocorrect torify'
+setopt correct_all
+export CORRECT_IGNORE_FILE='.*'
 
