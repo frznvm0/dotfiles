0b2ac1035f51607bfc333a0a2a0bb22859ed473a
frznvm0 <frznvm0@gmail.com>
2014-11-30 17:05:41 +0100

    Clean up completion.zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -11,20 +11,10 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
 
-# Processes
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
-
-# Disable named directories completion
-zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
-cdpath=(.)
-
 # Hosts
-[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
-[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
+[[ -r ~/.ssh/known_hosts ]] && _ssh_hosts=(${${${${(f)"$(<$HOME/.ssh/known_hosts)"}:#[\|]*}%%\ *}%%,*}) || _ssh_hosts=()
 hosts=(
   "$_ssh_hosts[@]"
-  "$_etc_hosts[@]"
   `hostname`
   localhost
 )
@@ -32,5 +22,5 @@ zstyle ':completion:*:hosts' hosts $hosts
 
 # Load completion modules
 fpath=($HOME/.zsh/completions $fpath)
-autoload -U compinit
-compinit -i
+autoload -U compinit && compinit
+
